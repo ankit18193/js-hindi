68a2e8f83ccc6b267b97cdc7beab0d49a0d549ad
ankit18193 <ankityadav18193@gmail.com>
2024-05-29 21:39:42 +0000

    strings

---
+++ 01_basics/jaString.js
@@ -0,0 +1,11 @@
+let name="dobby "
+const name2="kutta "
+let name3="hai"
+console.log(name+name2+name3);
+let newName=` hey `
+console.log(newName);
+console.log(`hello ${name}`);
+const newString= new String(`chaiOrCode`)
+console.log(newString.length);
+console.log(newString.toUpperCase());
+console.log(newString.indexOf('o'));
